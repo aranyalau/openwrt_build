17c45007b928a7441f5b54b267204d08943a7633
aranyalau <99336112+aranyalau@users.noreply.github.com>
2022-02-26 19:32:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git kenzok https://github.com/kenzok8/openwrt-packages' ~/lede/feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' ~/lede/feeds.conf.default
